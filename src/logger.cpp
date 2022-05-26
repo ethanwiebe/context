f95509efc23b80cfae8a744bfb1656bffea09837
Ethan <etwiebe55@gmail.com>
2022-05-25 18:30:20 -0700

    disabled debug logging when in release mode

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,11 +1,15 @@
 #include "logger.h"
 
 Logger::Logger(){
+#ifndef NDEBUG
 	logFile = std::fstream("c.log",std::ios::app|std::ios::out);
+#endif
 }
 
 Logger::~Logger(){
+#ifndef NDEBUG
 	logFile.close();
+#endif
 }
 
 Logger logger;
--- src/logger.h
+++ src/logger.h
@@ -12,38 +12,50 @@ public:
 	std::fstream logFile;
 
 	Logger& operator<<(char c){
+#ifndef NDEBUG
 		logFile << c;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
 	Logger& operator<<(s32 i){
+#ifndef NDEBUG
 		logFile << i;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
 	Logger& operator<<(s64 i){
+#ifndef NDEBUG
 		logFile << i;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
 	Logger& operator<<(const char* c){
+#ifndef NDEBUG
 		logFile << c;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
 	Logger& operator<<(const std::string& s){
+#ifndef NDEBUG
 		logFile << s;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
 	Logger& operator<<(std::string_view sv){
+#ifndef NDEBUG
 		logFile << sv;
 		logFile.flush();
+#endif
 		return *this;
 	}
 
